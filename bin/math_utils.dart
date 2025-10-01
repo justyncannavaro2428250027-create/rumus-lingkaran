e50cd0921094643631cec147fdd7b393077342c5
Justyn <justyncannavaro_2428250027@mhs.mdp.ac.id>
2025-10-01 11:08:23 +0700

    Tambah Fungsi Luas Persegi Panjang

---
+++ bin/math_utils.dart
@@ -0,0 +1,6 @@
+class MathUtils {
+  // Menghitung Luas Persegi Panjang
+  static double luasPersegiPanjang(double panjang, double lebar){
+    return panjang * lebar;
+  }
+}
--- bin/pertemuan_7.dart
+++ bin/pertemuan_7.dart
@@ -1,8 +1,9 @@
+import 'math_utils.dart';
 void main() {
   print('=== Latihan Rumus Matematika ===!');
 
 // Latihan dasar luas
-print('Luas Persegi Panjang (5 x 3 = 15)');
+print('Luas Persegi Panjang (5 x 3 = ${MathUtils.luasPersegiPanjang(5,3)})');
 print('Luas Segitiga (alas 6, Tinggi 4) = 12');
 print('Luas Lingkaran (r = 7) 154');
 
