4dce749591f50f2dbcad0c1d06652d039407c2ea
Justyn <justyncannavaro_2428250027@mhs.mdp.ac.id>
2025-10-01 10:37:09 +0700

    inisialisasi project matematika

---
+++ bin/pertemuan_7.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('=== Latihan Rumus Matematika ===!');
+
+// Latihan dasar luas
+print('Luas Persegi Panjang (5 x 3 = 15)');
+print('Luas Segitiga (alas 6, Tinggi 4) = 12');
+print('Luas Lingkaran (r = 7) 154');
+
+//Keliling
+print('Keliling Lingkaran (r = 7) = 44');
+}
